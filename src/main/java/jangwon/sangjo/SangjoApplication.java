c2d0b49b72ea0dc985e349a855375abd64232927
LeeHuSung <mn040820@naver.com>
2024-05-04 19:46:21 +0900

    first commit

---
+++ src/main/java/jangwon/sangjo/SangjoApplication.java
@@ -0,0 +1,13 @@
+package jangwon.sangjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangjoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jangwon/sangjo/SangjoApplicationTests.java
@@ -0,0 +1,13 @@
+package jangwon.sangjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
